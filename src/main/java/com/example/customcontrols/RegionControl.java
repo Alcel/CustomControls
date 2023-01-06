9705ccbdeb952ea03fd7d89ef2c4290bd66d001c
Alcel <104622578+Alcel@users.noreply.github.com>
2023-01-06 13:41:26 +0100

    Parte 6 Boton con animacion

--- src/main/java/com/example/customcontrols/RegionControl.java
+++ src/main/java/com/example/customcontrols/RegionControl.java
@@ -19,29 +19,29 @@ import java.util.function.Consumer;
 public class RegionControl extends Region {
     public enum Type { CLOSE, MINIMIZE, ZOOM }
 
-    private static final double               PREFERRED_WIDTH       = 12;
-    private static final double               PREFERRED_HEIGHT      = 12;
-    private static final double               MINIMUM_WIDTH         = 12;
-    private static final double               MINIMUM_HEIGHT        = 12;
-    private static final double               MAXIMUM_WIDTH         = 12;
-    private static final double               MAXIMUM_HEIGHT        = 12;
-    private static final PseudoClass          CLOSE_PSEUDO_CLASS    = PseudoClass.getPseudoClass("close");
-    private static final PseudoClass          MINIMIZE_PSEUDO_CLASS = PseudoClass.getPseudoClass("minimize");
-    private static final PseudoClass          ZOOM_PSEUDO_CLASS     = PseudoClass.getPseudoClass("zoom");
-    private static final PseudoClass          HOVERED_PSEUDO_CLASS  = PseudoClass.getPseudoClass("hovered");
-    private static final PseudoClass          PRESSED_PSEUDO_CLASS  = PseudoClass.getPseudoClass("pressed");
-    private static final PseudoClass          STATE_PSEUDO_CLASS    = PseudoClass.getPseudoClass("state");
-    private              BooleanProperty      hovered;
-    private              BooleanProperty      state;
-    private static       String               userAgentStyleSheet;
-    private              ObjectProperty<Type> type;
-    private              double               size;
-    private              double               width;
-    private              double               height;
-    private              Circle               circle;
-    private              Region               symbol;
-    private              Consumer<MouseEvent> mousePressedConsumer;
-    private              Consumer<MouseEvent> mouseReleasedConsumer;
+    private static final double PREFERRED_WIDTH = 12;
+    private static final double PREFERRED_HEIGHT = 12;
+    private static final double MINIMUM_WIDTH = 12;
+    private static final double MINIMUM_HEIGHT = 12;
+    private static final double MAXIMUM_WIDTH = 12;
+    private static final double MAXIMUM_HEIGHT = 12;
+    private static final PseudoClass CLOSE_PSEUDO_CLASS = PseudoClass.getPseudoClass("close");
+    private static final PseudoClass MINIMIZE_PSEUDO_CLASS = PseudoClass.getPseudoClass("minimize");
+    private static final PseudoClass ZOOM_PSEUDO_CLASS = PseudoClass.getPseudoClass("zoom");
+    private static final PseudoClass HOVERED_PSEUDO_CLASS = PseudoClass.getPseudoClass("hovered");
+    private static final PseudoClass PRESSED_PSEUDO_CLASS = PseudoClass.getPseudoClass("pressed");
+    private static final PseudoClass STATE_PSEUDO_CLASS = PseudoClass.getPseudoClass("state");
+    private BooleanProperty hovered;
+    private BooleanProperty state;
+    private static String userAgentStyleSheet;
+    private ObjectProperty<Type> type;
+    private double size;
+    private double width;
+    private double height;
+    private Circle circle;
+    private Region symbol;
+    private Consumer<MouseEvent> mousePressedConsumer;
+    private Consumer<MouseEvent> mouseReleasedConsumer;
 
 
     // ******************** Constructors **************************************
